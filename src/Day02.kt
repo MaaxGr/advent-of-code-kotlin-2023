c8a773e65a8bff67c8cdb55d7794b466d86c763b
Max <dev@maax.gr>
2023-12-03 21:02:34 +0100

    feat(Day03): Add first try

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,3 +1,5 @@
+
+
 fun main() {
     fun part1(input: List<String>): Int {
         return input
---
+++ src/Day03.kt
@@ -0,0 +1,132 @@
+fun main() {
+    val day03 = Day03()
+
+    fun part1(input: List<String>): Long {
+        with(day03) {
+            val game = parseGame(input)
+
+            return game.potentialPartNumbers
+                .filter { game.isRealPartNumber(it) }
+                .sumOf { it.number.toLong() }
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val testInput = readInput("Day03_test")
+    println("Part 1: ${part1(testInput)}")
+
+    val input = readInput("Day03_example")
+    println("Part 2: ${part2(input)}")
+}
+
+class Day03 {
+
+    data class Game(
+        val rows: List<String>,
+        val potentialPartNumbers: List<PotentialPartNumber>,
+        val lastColumnIndex: Int,
+        val lastRowIndex: Int,
+    )
+
+    data class PotentialPartNumber(
+        val number: Int,
+        val rowIndex: Int,
+        val columnStartIndex: Int,
+        val columnEndIndex: Int
+    )
+
+    fun Game.getSymbolAt(y: Int, x: Int): Char {
+        return rows[y].elementAt(x)
+    }
+
+    fun Char.isValidSymbol(): Boolean {
+        return this != '.'
+    }
+
+    fun Game.isRealPartNumber(potentialPartNumber: PotentialPartNumber): Boolean {
+        val symbolLeftPresent =
+            if (potentialPartNumber.columnStartIndex == 0) false else getSymbolAt(potentialPartNumber.rowIndex, potentialPartNumber.columnStartIndex - 1).isValidSymbol()
+        val symbolRightPresent =
+            if (potentialPartNumber.columnEndIndex == lastColumnIndex) false else getSymbolAt(potentialPartNumber.rowIndex, potentialPartNumber.columnEndIndex + 1).isValidSymbol()
+
+        val symbolTopPresent = if (potentialPartNumber.rowIndex == 0) false else {
+            var isValidSymbol = false
+            for (index in potentialPartNumber.columnStartIndex..potentialPartNumber.columnEndIndex) {
+                if (getSymbolAt(potentialPartNumber.rowIndex - 1, index).isValidSymbol()) {
+                    isValidSymbol = true
+                }
+            }
+            isValidSymbol
+        }
+
+        val symbolBottomPresent = if (potentialPartNumber.rowIndex == rows.size - 1) false else {
+            var isValidSymbol = false
+            for (index in potentialPartNumber.columnStartIndex..potentialPartNumber.columnEndIndex) {
+                if (getSymbolAt(potentialPartNumber.rowIndex + 1, index).isValidSymbol()) {
+                    isValidSymbol = true
+                }
+            }
+            isValidSymbol
+        }
+
+        val symbolTopLeftDiagonalPresent = if (potentialPartNumber.rowIndex == 0 || potentialPartNumber.columnStartIndex == 0) false
+        else getSymbolAt( potentialPartNumber.rowIndex - 1, potentialPartNumber.columnStartIndex - 1).isValidSymbol()
+
+        val symbolTopRightDiagonalPresent = if (potentialPartNumber.rowIndex == 0 || potentialPartNumber.columnEndIndex == lastColumnIndex) false
+        else getSymbolAt( potentialPartNumber.rowIndex - 1, potentialPartNumber.columnEndIndex + 1).isValidSymbol()
+
+        val symbolBottomLeftDiagonalPresent = if (potentialPartNumber.rowIndex == rows.size - 1 || potentialPartNumber.columnStartIndex == 0) false
+        else getSymbolAt( potentialPartNumber.rowIndex + 1, potentialPartNumber.columnStartIndex - 1).isValidSymbol()
+
+        val symbolBottomRightDiagonalPresent = if (potentialPartNumber.rowIndex == rows.size - 1 || potentialPartNumber.columnEndIndex == lastColumnIndex) false
+        else getSymbolAt( potentialPartNumber.rowIndex + 1, potentialPartNumber.columnEndIndex + 1).isValidSymbol()
+
+        return symbolLeftPresent || symbolRightPresent || symbolTopPresent || symbolBottomPresent
+                || symbolTopLeftDiagonalPresent || symbolTopRightDiagonalPresent
+                || symbolBottomLeftDiagonalPresent || symbolBottomRightDiagonalPresent
+    }
+
+    fun parseGame(rows: List<String>): Game {
+        val potentialPartNumbers = mutableListOf<PotentialPartNumber>()
+
+        rows.forEachIndexed { rowIndex, row ->
+            var numberBuffer = ""
+            var numberStartColumnIndex = 0
+
+            row.forEachIndexed { characterIndex, character ->
+                if (character.digitToIntOrNull() != null) {
+                    if (numberBuffer.isBlank()) {
+                        numberStartColumnIndex = characterIndex
+                    }
+
+                    numberBuffer += character
+                } else {
+                    if (numberBuffer.isNotBlank()) {
+                        potentialPartNumbers.add(
+                            PotentialPartNumber(
+                                number = numberBuffer.toInt(),
+                                rowIndex = rowIndex,
+                                columnStartIndex = numberStartColumnIndex,
+                                columnEndIndex = characterIndex - 1
+                            )
+                        )
+                        numberBuffer = ""
+                    }
+                }
+            }
+        }
+
+        return Game(
+            rows = rows,
+            potentialPartNumbers = potentialPartNumbers.toList(),
+            lastRowIndex = rows.size - 1,
+            lastColumnIndex = rows[0].length - 1
+        )
+
+    }
+
+}
+
